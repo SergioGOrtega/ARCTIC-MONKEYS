9d6054b50f5ac49a2f3ee4dc268b639c593d4ce3
Sergio Gamez <sergiovdg2016@gmail.com>
2023-12-15 19:17:13 +0100

    Comentarios de código

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+/*****************************************************************************/
+/*                     JavaScript de index.html                              */
+/*                     Autor: Sergio Gamez Ortega                            */
+/*                     Version: 1.0                                          */
+/*                     Fecha: Octubre de 2023                                */
+/*                     Comentario: Proyecto de Lenguaje de Marcas            */
+/*****************************************************************************/
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
